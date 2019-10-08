86f122fa7288ecf788f43c18dd2aa55d659dfd7d
Ivan Estrada <gakuseivan@gmail.com>
2019-10-07 21:37:42 -0300

    Estructura del Toast

--- app/src/main/java/com/example/mytoastivan/MainActivity.java
+++ app/src/main/java/com/example/mytoastivan/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView myview = (TextView) findViewById(R.id.btnDefault);
+        myview.setText("Modificando desde Java");
 
         View boton1 = findViewById(R.id.btnDefault);
         boton1.setOnClickListener(this);
@@ -39,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
          //toast.setGravity(Gravity.TOP|Gravity.LEFT, 0,0);
          //toast.show();
      }
-        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show();
     }
 }
 
